e6246eb7830702940c62def14c0b515e82dd6c93
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-14 04:06:40 +0200

    create spring Request mapping for get list of all videos that was saved.

--- src/main/java/org/magnum/dataup/MyVideoController.java
+++ src/main/java/org/magnum/dataup/MyVideoController.java
@@ -24,6 +24,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.magnum.dataup.model.Video;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -62,4 +65,11 @@ public class MyVideoController {
 		return base;
 	}
 	
+	// Controller for Get all videos saved in app.
+  	@RequestMapping(value = "/video", method = RequestMethod.GET)
+  	public @ResponseBody Collection<Video> getVideoList(){
+		return videos.values();
+  	}
+  	
+  	
 }
